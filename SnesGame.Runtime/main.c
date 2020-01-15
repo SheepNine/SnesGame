ea217a3694d9c7ea67c43a7f6ac43137264f3888
SheepNine <sheepnine@gmail.com>
2020-01-14 22:45:19 -0500

    Acquire the window surface and paint to it

--- SnesGame.Runtime/main.c
+++ SnesGame.Runtime/main.c
@@ -11,14 +11,31 @@ int main(int argc, char** argv) {
 				640, 480,
 				0);
 		if (window != NULL) {
-			SDL_bool loop = SDL_TRUE;
-			SDL_Event event;
-			while (loop) {
-				SDL_PollEvent(&event);
-				switch (event.type) {
-				case SDL_QUIT:
-					loop = SDL_FALSE;
-					break;
+			SDL_Surface* surface = SDL_GetWindowSurface(window);
+			if (surface != NULL) {
+				// Error check the lock/unlock/update
+				// Check the window surface format just in case it isn't 8bpp ARGB
+				SDL_LockSurface(surface);
+				for (int y = 0; y < 480; y++) {
+					for (int x = 0; x < 640; x++) {
+						int i = 640 * y + x;
+						Uint32 r = (255 * x) / 639;
+						Uint32 b = (255 * y) / 479;
+						((Uint32*)surface->pixels)[i] = (r << 16) | (b);
+					}
+				}
+				SDL_UnlockSurface(surface);
+				SDL_UpdateWindowSurface(window);
+
+				SDL_bool loop = SDL_TRUE;
+				SDL_Event event;
+				while (loop) {
+					SDL_PollEvent(&event);
+					switch (event.type) {
+					case SDL_QUIT:
+						loop = SDL_FALSE;
+						break;
+					}
 				}
 			}
 			SDL_DestroyWindow(window);
