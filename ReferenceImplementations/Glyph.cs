1a24b38169d7ef906a1bad45e007fd1ac1ff19ea
SheepNine <sheepnine@gmail.com>
2020-04-11 04:13:16 -0400

    Add TDD'd C# reference glyph implementation

---
+++ ReferenceImplementations/Glyph.cs
@@ -0,0 +1,66 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using System;
+
+namespace ReferenceImplementations
+{
+    class Glyph
+    {
+        private byte[] data;
+
+        public Glyph()
+        {
+            data = Enumerable.Repeat((byte)0, 32).ToArray();
+        }
+
+        public int GetIndex(int x, int y)
+        {
+            if (x < 0 || x >= 8)
+                throw new ArgumentOutOfRangeException("x");
+            if (y < 0 || y >= 8)
+                throw new ArgumentOutOfRangeException("y");
+
+            var mask = (byte)(0x1 << x);
+            var result = (byte)0;
+            if ((data[4 * y + 0] & mask) != 0)
+                result |= 0x1;
+            if ((data[4 * y + 1] & mask) != 0)
+                result |= 0x2;
+            if ((data[4 * y + 2] & mask) != 0)
+                result |= 0x4;
+            if ((data[4 * y + 3] & mask) != 0)
+                result |= 0x8;
+
+            return result;
+        }
+
+        public void SetIndex(int x, int y, int value)
+        {
+            if (x < 0 || x >= 8)
+                throw new ArgumentOutOfRangeException("x");
+            if (y < 0 || y >= 8)
+                throw new ArgumentOutOfRangeException("y");
+            if (value < 0 || value > 0xF)
+                throw new ArgumentOutOfRangeException("value");
+
+            var mask = (byte)(0x1 << x);
+
+            if ((value & 0x1) != 0)
+                data[4 * y + 0] |= mask;
+            if ((value & 0x2) != 0)
+                data[4 * y + 1] |= mask;
+            if ((value & 0x4) != 0)
+                data[4 * y + 2] |= mask;
+            if ((value & 0x8) != 0)
+                data[4 * y + 3] |= mask;
+        }
+
+        public IEnumerable<byte> Bytes
+        {
+            get
+            {
+                return data;
+            }
+        }
+    }
+}
---
+++ ReferenceImplementations/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Linq;
+
+namespace ReferenceImplementations
+{
+    class TestHarness
+    {
+        static void Main(string[] args)
+        {
+            GlyphTests.AllZero();
+            GlyphTests.AllOne();
+            GlyphTests.AllTwo();
+            GlyphTests.AllFour();
+            GlyphTests.AllEight();
+            GlyphTests.AllFifteen();
+            GlyphTests.DiagonalStripe();
+        }
+    }
+
+    class GlyphTests
+    {
+        private static void ExpectData(Glyph sut, string expected)
+        {
+            var actual = String.Join(string.Empty, sut.Bytes.Select(b => b.ToString("X2")));
+            expected = expected.Replace(" ", string.Empty);
+            if (!String.Equals(actual, expected))
+            {
+                System.Diagnostics.Debugger.Break();
+            }
+        }
+
+        private static void WriteAndRead(Glyph sut, int x, int y, int value)
+        {
+            sut.SetIndex(x, y, value);
+            if (sut.GetIndex(x, y) != value)
+                System.Diagnostics.Debugger.Break();
+        }
+
+        public static void AllZero()
+        {
+            var sut = new Glyph();
+            for (int y = 0; y < 8; y++)
+                for (int x = 0; x < 8; x++)
+                    WriteAndRead(sut, x, y, 0);
+
+            ExpectData(sut, "00 00 00 00  00 00 00 00  00 00 00 00  00 00 00 00  00 00 00 00  00 00 00 00  00 00 00 00  00 00 00 00");
+        }
+
+        public static void AllOne()
+        {
+            var sut = new Glyph();
+            for (int y = 0; y < 8; y++)
+                for (int x = 0; x < 8; x++)
+                    WriteAndRead(sut, x, y, 1);
+
+            ExpectData(sut, "FF 00 00 00  FF 00 00 00  FF 00 00 00  FF 00 00 00  FF 00 00 00  FF 00 00 00  FF 00 00 00  FF 00 00 00");
+        }
+
+        public static void AllTwo()
+        {
+            var sut = new Glyph();
+            for (int y = 0; y < 8; y++)
+                for (int x = 0; x < 8; x++)
+                    WriteAndRead(sut, x, y, 2);
+
+            ExpectData(sut, "00 FF 00 00  00 FF 00 00  00 FF 00 00  00 FF 00 00  00 FF 00 00  00 FF 00 00  00 FF 00 00  00 FF 00 00");
+        }
+
+        public static void AllFour()
+        {
+            var sut = new Glyph();
+            for (int y = 0; y < 8; y++)
+                for (int x = 0; x < 8; x++)
+                    WriteAndRead(sut, x, y, 4);
+
+            ExpectData(sut, "00 00 FF 00  00 00 FF 00  00 00 FF 00  00 00 FF 00  00 00 FF 00  00 00 FF 00  00 00 FF 00  00 00 FF 00");
+        }
+
+        public static void AllEight()
+        {
+            var sut = new Glyph();
+            for (int y = 0; y < 8; y++)
+                for (int x = 0; x < 8; x++)
+                    WriteAndRead(sut, x, y, 8);
+
+            ExpectData(sut, "00 00 00 FF  00 00 00 FF  00 00 00 FF  00 00 00 FF  00 00 00 FF  00 00 00 FF  00 00 00 FF  00 00 00 FF");
+        }
+
+        public static void AllFifteen()
+        {
+            var sut = new Glyph();
+            for (int y = 0; y < 8; y++)
+                for (int x = 0; x < 8; x++)
+                    WriteAndRead(sut, x, y, 15);
+
+            ExpectData(sut, "FF FF FF FF  FF FF FF FF  FF FF FF FF  FF FF FF FF  FF FF FF FF  FF FF FF FF  FF FF FF FF  FF FF FF FF");
+        }
+
+        public static void DiagonalStripe()
+        {
+            var sut = new Glyph();
+            for (int i = 0; i < 8; i++)
+                WriteAndRead(sut, i, i, 15);
+
+            ExpectData(sut, "01 01 01 01  02 02 02 02  04 04 04 04  08 08 08 08  10 10 10 10  20 20 20 20  40 40 40 40  80 80 80 80");
+        }
+    }
+}
