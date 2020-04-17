9c3b8e6ba21f0bf3d577d92e84c53a261a1a3c26
SheepNine <sheepnine@gmail.com>
2020-04-17 08:49:28 -0400

    Implement dynamic scaling of pixels to available screen size

--- SnesGame.Runtime/main.c
+++ SnesGame.Runtime/main.c
@@ -20,12 +20,27 @@ void readGlyphList(char *filename, Uint8 *glyphlist) {
 	}
 }
 
-void bbBlit(Uint32* dest) {
+void bbBlit(SDL_Surface* surface) {
+	int scale = SDL_min(surface->w, surface->h) / 248;
+	if (scale < 1) {
+		return;
+	}
+
+	int xSkip = (surface->w - scale * 248) / 2;
+	int ySkip = (surface->h - scale * 248) / 2;
+
+	Uint32* dest = (Uint32*)surface->pixels + ySkip * surface->pitch / 4 + xSkip;
 	int i = 0;
 	for (int y = 0; y < 248; y++) {
-		for (int x = 0; x < 248; x++) {
-			int i = (y * 248) + x;
-			dest[i] = getDot_BB(bb, x, y);
+		for (int v = 0; v < scale; v++) {
+			Uint32* writPtr = dest;
+			for (int x = 0; x < 248; x++) {
+				for (int u = 0; u < scale; u++) {
+					*writPtr = getDot_BB(bb, x, y);
+					writPtr += 1;
+				}
+			}
+			dest += surface->pitch / 4;
 		}
 	}
 }
@@ -117,51 +132,39 @@ int main(int argc, char** argv) {
 		}
 	}
 
-	scan_PPU(ppu, bb);
-
 	if (SDL_Init(SDL_INIT_VIDEO) == 0) {
 		SDL_Window* window = SDL_CreateWindow(
 				"SnesGame",
 				SDL_WINDOWPOS_CENTERED,
 				SDL_WINDOWPOS_CENTERED,
-				248, 248,
-				0);
+				248 * 3, 248 * 3,
+				SDL_WINDOW_RESIZABLE);
 		if (window != NULL) {
-			SDL_Surface* surface = SDL_GetWindowSurface(window);
-			if (surface != NULL) {
-				// Error check the lock/unlock/update
-				// Check the window surface format just in case it isn't 8bpp ARGB
+			// Error check the lock/unlock/update
+			// Check the window surface format just in case it isn't 8bpp ARGB
 
-				SDL_LockSurface(surface);
-				bbBlit((Uint32*)surface->pixels);
-				SDL_UnlockSurface(surface);
-				SDL_UpdateWindowSurface(window);
+			SDL_AddTimer(20, heartbeatCallback, NULL);
+			int x = 0;
+			int y = 0;
 
-				SDL_AddTimer(20, heartbeatCallback, NULL);
-				int x = 0;
-				int y = 0;
-
-				SDL_bool loop = SDL_TRUE;
-				SDL_Event event;
-				while (loop) {
-					SDL_WaitEvent(&event);
-					switch (event.type) {
-					case SDL_QUIT:
-						loop = SDL_FALSE;
-						break;
-					case SDL_USEREVENT:
-						x = (x + 7) % (R_WIDTH * 2);
-						y = (y + 12) % (R_HEIGHT * 2);
-						int dX = x >= R_WIDTH ? (R_WIDTH * 2 - 1) - x : x;
-						int dY = y >= R_HEIGHT ? (R_HEIGHT * 2 - 1) - y : y;
-						setDot_BB(bb, dX, dY, 0x80, 0x80, 0x80, SDL_TRUE);
-
+			SDL_bool loop = SDL_TRUE;
+			SDL_Event event;
+			while (loop) {
+				SDL_WaitEvent(&event);
+				switch (event.type) {
+				case SDL_QUIT:
+					loop = SDL_FALSE;
+					break;
+				case SDL_USEREVENT:
+					scan_PPU(ppu, bb);
+					SDL_Surface* surface = SDL_GetWindowSurface(window);
+					if (surface != NULL) {
 						SDL_LockSurface(surface);
-						bbBlit((Uint32*)surface->pixels);
+						bbBlit(surface);
 						SDL_UnlockSurface(surface);
 						SDL_UpdateWindowSurface(window);
-						break;
 					}
+					break;
 				}
 			}
 			SDL_DestroyWindow(window);
