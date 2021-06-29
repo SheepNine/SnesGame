53257f197e857ba636372489659fc45df61f0a70
SheepNine <sheepnine@gmail.com>
2021-06-29 15:59:25 -0400

    Add WideBackdrop foundation class

--- SnesGame.Runtime/api/snes_foundation_classes.h
+++ SnesGame.Runtime/api/snes_foundation_classes.h
@@ -8,5 +8,10 @@ void setWorldLocus_CS(hCS cs, Sint32 x, Sint32 y);
 void setScreenLocus_CS(hCS cs, Uint8 x, Uint8 y);
 void worldToScreen_CS(hCS cs, Sint32 worldX, Sint32 worldY, Sint32* screenX, Sint32* screenY);
 
+typedef struct WB* hWB;
+hWB creat_WB(Uint16 width, Uint16 height, Uint8* data);
+void destr_WB(hWB wb);
+void draw_WB(hRENDER render, Uint8 backdropIndex, SDL_bool topmost, hWB wb, Sint16 x, Sint16 y, Uint8 bankIndex, Uint8 paletteIndex, Uint8 mask0);
+
 void drawBackdropString(hRENDER render, Uint8 backdropIndex, char* string, Uint8 x, Uint8 y, Uint8 bankIndex, Uint8 paletteIndex, SDL_bool mask0);
 void fillBackdrop(hRENDER render, Uint8 backdropIndex, Uint8 brushIndex, Uint8 bankIndex, Uint8 paletteIndex, SDL_bool hFlip, SDL_bool vFlip, SDL_bool mask0);
--- SnesGame.Runtime/snes_foundation_classes.c
+++ SnesGame.Runtime/snes_foundation_classes.c
@@ -48,3 +48,51 @@ void worldToScreen_CS(hCS cs, Sint32 worldX, Sint32 worldY, Sint32* screenX, Sin
 	*screenX = cs->screenLocusX + ((worldX - cs->worldLocusX + cs->worldLocusY - worldY + (1 << 11)) >> 12);
 	*screenY = cs->screenLocusY - ((worldX - cs->worldLocusX + worldY - cs->worldLocusY + (1 << 12)) >> 13);
 }
+
+
+struct WB {
+	Uint16 width;
+	Uint16 height;
+	Uint8* data;
+} WB;
+
+hWB creat_WB(Uint16 width, Uint16 height, Uint8* data) {
+	hWB result = (hWB)SDL_malloc(sizeof(WB));
+	result->width = width;
+	result->height = height;
+	result->data = SDL_malloc(width * height);
+	return result;
+}
+
+void destr_WB(hWB wb) {
+	SDL_free(wb->data);
+	SDL_free(wb);
+}
+
+
+void draw_WB(hRENDER render, Uint8 backdropIndex, SDL_bool topmost, hWB wb, Sint16 x, Sint16 y, Uint8 bankIndex, Uint8 paletteIndex, Uint8 mask0) {
+	Uint8 dX = (8 - x) & 0x7;
+	Uint8 dY = (8 - y) & 0x7;
+	setBackdropControl(render, backdropIndex, dX, dY, SDL_TRUE, topmost);
+
+	Sint16 srcX = (-x) >> 3;
+	Sint16 srcY = (-y) >> 3;
+
+	for (Uint8 y = 0; y < 32; y++)
+	{
+		for (Uint8 x = 0; x < 32; x++)
+		{
+			Uint8 brushIndex;
+			if (srcX >= 0 && srcX < wb->width && srcY >= 0 && srcY < wb->height) {
+				brushIndex = wb->data[wb->width * srcY + srcX];
+			}
+			else {
+				brushIndex = 0;
+			}
+			setBackdropStroke(render, backdropIndex, x, y, brushIndex, bankIndex, paletteIndex, SDL_FALSE, SDL_FALSE, mask0);
+			srcX += 1;
+		}
+		srcX -= 32;
+		srcY += 1;
+	}
+}
