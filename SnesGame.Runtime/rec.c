c2546df5de2d052bf93228bdac039c83322ec15e
SheepNine <sheepnine@gmail.com>
2021-09-06 16:16:22 -0400

    Implement demo recording

--- SnesGame.Runtime/rec.c
+++ SnesGame.Runtime/rec.c
@@ -4,36 +4,55 @@ struct REC {
 	hGP gp;
 	Uint16 currentValue;
 	Uint8 currentCount;
+	SDL_RWops* RW;
 } REC;
 
 hREC creat_REC(hGP gp, char* filename) {
-	filename; // TEMPORARY BUILD FIX
 	hREC result = (hREC)SDL_malloc(sizeof(REC));
 	result->gp = gp;
 	result->currentValue = 0;
 	result->currentCount = 0;
+	if (filename != NULL) {
+		result->RW = SDL_RWFromFile(filename, "w");
+		// TODO: add some sort of header
+	}
 	return result;
 }
 
 void emitRun_REC(hREC rec) {
-	if (rec->currentCount == 0) {
+	if (rec->RW == NULL || rec->currentCount == 0) {
 		return;
 	}
 
-	// Write it out
+	Uint8 data[3];
+	data[0] = rec->currentValue >> 4;
+	data[1] = rec->currentValue & 0xFF;
+
+	if (rec->currentCount < 0x8) {
+		data[0] |= rec->currentCount << 4;
+		SDL_RWwrite(rec->RW, data, 1, 2);
+	}
+	else {
+		data[0] |= 0x80;
+		data[0] |= 0x70 & (rec->currentCount << 4);
+		data[2] = (rec->currentCount >> 3);
+		SDL_RWwrite(rec->RW, data, 1, 3);
+	}
 
 	rec->currentCount = 0;
 }
 
 void destr_REC(hREC rec) {
 	emitRun_REC(rec);
+	if (rec->RW != NULL) {
+		SDL_RWclose(rec->RW);
+	}
 	SDL_free(rec);
-
 }
 
 void record_REC(hREC rec) {
 	Uint16 nextValue = getCurrent_GP(rec->gp);
-	if (rec->currentCount == 127 || rec->currentValue != nextValue) {
+	if (rec->currentCount == 0x7FF || rec->currentValue != nextValue) {
 		emitRun_REC(rec);
 	}
 	rec->currentValue = nextValue;
