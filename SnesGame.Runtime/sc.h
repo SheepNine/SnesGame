78259955a8e1cf00072ff66ac7bbf6c5011fde43
SheepNine <sheepnine@gmail.com>
2021-10-17 22:16:27 -0400

    Set up SC to be imported into interop library

--- SnesGame.Runtime/sc.h
+++ SnesGame.Runtime/sc.h
@@ -2,16 +2,20 @@
 #include "SDL.h"
 #include "api/snes_api.h"
 
+#ifndef INTEROP_EXTERN
+#define INTEROP_EXTERN
+#endif
+
 typedef struct SC* hSC;
 
-hSC creat_SC();
-void destr_SC(hSC sc);
+INTEROP_EXTERN hSC creat_SC();
+INTEROP_EXTERN void destr_SC(hSC sc);
 
-void playNote_SC(hSC sc,
+INTEROP_EXTERN void playNote_SC(hSC sc,
 	Uint16 length,
 	Uint8 volumeLow, Uint8 volumeHigh, ShiftDir volumeShift, Uint8 volumeShiftSpeed, EdgeBehaviour volumeEB,
 	Uint16 periodLow, Uint16 periodHigh, ShiftDir periodShift, Uint8 periodShiftSpeed, EdgeBehaviour periodEB);
 
-void silence_SC(hSC sc);
+INTEROP_EXTERN void silence_SC(hSC sc);
 
-Sint16 getNextSample_SC(hSC sc);
+INTEROP_EXTERN Sint16 getNextSample_SC(hSC sc);
