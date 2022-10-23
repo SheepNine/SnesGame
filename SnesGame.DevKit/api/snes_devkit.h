0c25b8ddb01de2685f769801ac506b4989f21380
SheepNine <sheepnine@gmail.com>
2022-10-22 21:13:24 -0400

    Check code line lengths as part of build

--- SnesGame.DevKit/api/snes_devkit.h
+++ SnesGame.DevKit/api/snes_devkit.h
@@ -15,8 +15,10 @@ hWB creat_WB(Uint16 width, Uint16 height, Uint8* data);
 void destr_WB(hWB wb);
 Uint16 getWidth_WB(hWB wb);
 Uint16 getHeight_WB(hWB wb);
-void draw_WB(hRENDER render, Uint8 backdropIndex, SDL_bool topmost, hWB wb, Sint16 x, Sint16 y, Uint8 bankIndex, Uint8 paletteIndex, Uint8 mask0);
-void appendToBackdrop_WB(hRENDER render, Uint8 backdropIndex, hWB wb, Sint16 tileX, Sint16 tileY, Uint8 bankIndex, Uint8 paletteIndex, Uint8 mask0);
+void draw_WB(hRENDER render, Uint8 backdropIndex, SDL_bool topmost, hWB wb, Sint16 x, Sint16 y,
+    Uint8 bankIndex, Uint8 paletteIndex, Uint8 mask0);
+void appendToBackdrop_WB(hRENDER render, Uint8 backdropIndex, hWB wb, Sint16 tileX, Sint16 tileY,
+    Uint8 bankIndex, Uint8 paletteIndex, Uint8 mask0);
 
 typedef struct Dialog* hDialog;
 hDialog creatBare_Dialog(char* text, Uint8 height, SDL_bool tallText);
@@ -26,11 +28,14 @@ SDL_bool isFinished_Dialog(hDialog dialog);
 SDL_bool pageDone_Dialog(hDialog dialog);
 void poke_Dialog(hDialog dialog);
 void update_Dialog(hDialog dialog);
-void renderText_Dialog(hDialog dialog, hRENDER render, Uint8 layerIndex, Uint8 bankIndex, Uint8 paletteIndex);
-void renderBorder_Dialog(hDialog dialog, hRENDER render, Uint8 layerIndex, Uint8 bankIndex, Uint8 paletteIndex);
+void renderText_Dialog(hDialog dialog, hRENDER render, Uint8 layerIndex,
+    Uint8 bankIndex, Uint8 paletteIndex);
+void renderBorder_Dialog(hDialog dialog, hRENDER render, Uint8 layerIndex,
+    Uint8 bankIndex, Uint8 paletteIndex);
 
 typedef struct Record* hRecord;
-hRecord creat_Record(Uint8 trackCount, Uint8* opcodeStream, Uint16* trackOffsets, Uint16* trackLengths);
+hRecord creat_Record(Uint8 trackCount, Uint8* opcodeStream, Uint16* trackOffsets,
+    Uint16* trackLengths);
 void destr_Record(hRecord rec);
 
 typedef struct Jukebox* hJukebox;
@@ -43,18 +48,23 @@ void playBgm_Jukebox(hJukebox jukebox, hRecord recording);
 void stopBgm_Jukebox(hJukebox jukebox);
 void startBgm_Jukebox(hJukebox jukebox);
 
-void drawBackdropString(hRENDER render, Uint8 backdropIndex, char* string, Uint8 x, Uint8 y, Uint8 bankIndex, Uint8 paletteIndex, SDL_bool mask0);
-void fillBackdrop(hRENDER render, Uint8 backdropIndex, Uint8 brushIndex, Uint8 bankIndex, Uint8 paletteIndex, SDL_bool hFlip, SDL_bool vFlip, SDL_bool mask0);
+void drawBackdropString(hRENDER render, Uint8 backdropIndex, char* string, Uint8 x, Uint8 y,
+    Uint8 bankIndex, Uint8 paletteIndex, SDL_bool mask0);
+void fillBackdrop(hRENDER render, Uint8 backdropIndex, Uint8 brushIndex,
+    Uint8 bankIndex, Uint8 paletteIndex, SDL_bool hFlip, SDL_bool vFlip, SDL_bool mask0);
 
 void setPaletteColor(Uint8* data, Uint8 colorIndex, Uint8 r, Uint8 g, Uint8 b, SDL_bool t);
 
-typedef Uint8(*hSBRC)(hRENDER render, Uint8 actorIndex, Sint32 screenX, Sint32 screenY, void* renderContext);
+typedef Uint8(*hSBRC)(hRENDER render, Uint8 actorIndex, Sint32 screenX, Sint32 screenY,
+    void* renderContext);
 
 typedef struct SB* hSB;
 hSB creat_SB(hCC cc);
 void destr_SB(hSB sb);
 
-void addSprite_SB(hSB sb, Sint32 worldX, Sint32 worldY, hSBRC renderCallback, void* renderContext);
+void addSprite_SB(hSB sb, Sint32 worldX, Sint32 worldY, hSBRC renderCallback,
+    void* renderContext);
 Uint8 render_SB(hSB sb, hRENDER render, Uint8 actorIndex);
-void addSingleBrushSprite_SB(hSB sb, Sint32 worldX, Sint32 worldY, Uint8 layer, Uint8 brushIndex, Uint8 bankIndex,
-    Uint8 paletteIndex, Sint8 centerX, Sint8 centerY, Uint8 sizeX, Uint8 sizeY, SDL_bool hFlip, SDL_bool vFlip, SDL_bool mask0);
+void addSingleBrushSprite_SB(hSB sb, Sint32 worldX, Sint32 worldY, Uint8 layer,
+    Uint8 brushIndex, Uint8 bankIndex, Uint8 paletteIndex, Sint8 centerX, Sint8 centerY,
+    Uint8 sizeX, Uint8 sizeY, SDL_bool hFlip, SDL_bool vFlip, SDL_bool mask0);
